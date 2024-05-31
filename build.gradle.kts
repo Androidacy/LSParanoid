c81a5b674fbab85358483023bb5a4a8f6c504a3a
Androidacy Service Account <opensource@androidacy.com>
2024-05-31 14:07:09 -0400

    chore: update build dependencies (master)...
    chore: update build dependencies (master)
    
    - Bump project version to 0.6.5
    - Update AGP to 8.4.1 and Kotlin to 2.0.0
    - Upgrade asm-commons to 9.7
    - Update Gradle wrapper to 8.7
    
    Signed-off-by: Androidacy Service Account <opensource@androidacy.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ plugins {
 
 allprojects {
     group = "com.github.Androidacy.LSParanoid"
-    version = "0.6.4"
+    version = "0.6.5"
 
     plugins.withType(JavaPlugin::class.java) {
         extensions.configure(JavaPluginExtension::class.java) {
