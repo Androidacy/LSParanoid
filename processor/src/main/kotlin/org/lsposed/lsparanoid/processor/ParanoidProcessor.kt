9e5eb280ced56a0512d87019d687e308441a1b82
Androidacy Service Account <opensource@androidacy.com>
2024-06-18 18:40:32 -0400

    fix the last fix
    this fix should fix the fix that did not fix the issue which is fixed now
    
    Signed-off-by: Androidacy Service Account <opensource@androidacy.com>

--- processor/src/main/kotlin/org/lsposed/lsparanoid/processor/ParanoidProcessor.kt
+++ processor/src/main/kotlin/org/lsposed/lsparanoid/processor/ParanoidProcessor.kt
@@ -106,7 +106,7 @@ class ParanoidProcessor(
 
     private fun createDeobfuscator(): Deobfuscator {
         val deobfuscatorInternalName =
-            "org/lsposed/LSParanoid/Deobfuscator${composeDeobfuscatorNameSuffix()}"
+            "org/lsposed/lsparanoid/Deobfuscator${composeDeobfuscatorNameSuffix()}"
         val deobfuscatorType = getObjectTypeByInternalName(deobfuscatorInternalName)
         val deobfuscationMethod =
             Method("getString", Type.getType(String::class.java), arrayOf(Type.LONG_TYPE))
