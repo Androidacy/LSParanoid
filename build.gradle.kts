6300187b0b667cdf2aaa5ae2bb2ba2765c7e6cc9
Androidacy Service Account <opensource@androidacy.com>
2024-06-18 18:38:56 -0400

    Fix: Update deobfuscator internal name
    The deobfuscator internal name was incorrectly using the an updated package name "org/Androidacy". This commit
     updates it to the correct package name "org/lsposed".
    
    Signed-off-by: Androidacy Service Account <opensource@androidacy.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ plugins {
 
 allprojects {
     group = "com.github.Androidacy.LSParanoid"
-    version = "0.6.7"
+    version = "0.6.8"
 
     plugins.withType(JavaPlugin::class.java) {
         extensions.configure(JavaPluginExtension::class.java) {
