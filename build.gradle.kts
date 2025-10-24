6c8f950ba8f31784c193bdbdb3d4ea1c351e5c97
androidacy-user <opensource@androidacy.com>
2025-10-24 16:06:36 +0000

    chore: new ver
    Signed-off-by: androidacy-user <opensource@androidacy.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ plugins {
 
 allprojects {
     group = "com.androidacy.lsparanoid"
-    version = "0.9.16"
+    version = "0.9.17"
 
     plugins.withType(JavaPlugin::class.java) {
         extensions.configure(JavaPluginExtension::class.java) {
